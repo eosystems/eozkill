c4db5ac09a9cc08458d600e6be1581802b2b17ee
r_nishio <quick.xp@me.com>
2017-05-13 17:19:14 +0900

    elastic search

---
+++ app/models/elastic_zkill.rb
@@ -0,0 +1,38 @@
+class ElasticZkill
+  include ActiveModel::Model
+
+  @client = nil
+
+  def initialize
+    @client = Elasticsearch::Client.new({log: true, hosts: {host: 'localhost'}})
+  end
+
+  def health_check
+    @client.cluster.health
+  end
+
+  def all_index
+    @client.cat.indices
+  end
+
+  def create_index(name)
+    @client.indices.create(index: name)
+  end
+
+  def create_document(index_name, id, body)
+    @client.create({ index: index_name, type: 'info', id: id, body: body })
+  end
+
+  def search(index_name, query: '*')
+    @client.search(index: index_name, q: query)
+  end
+
+  def put_mapping(index_name, type, body)
+    @client.indices.put_mapping index: index_name, type: type, body: body
+  end
+
+  def delete_mapping(index_name, type)
+    @client.indices.delete_mapping index: index_name, type: type
+  end
+
+end
