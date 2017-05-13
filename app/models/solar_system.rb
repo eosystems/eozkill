565a7ed79f8623cc2d7602814c7fbf331ac63aac
r_nishio <quick.xp@me.com>
2017-05-13 23:47:39 +0900

    dev

--- app/models/solar_system.rb
+++ app/models/solar_system.rb
@@ -2,8 +2,9 @@
 #
 # Table name: solar_systems
 #
-#  id   :integer          not null, primary key
-#  name :string(255)      not null
+#  id        :integer          not null, primary key
+#  region_id :integer          not null
+#  name      :string(255)      not null
 #
 
 class SolarSystem < ApplicationRecord
