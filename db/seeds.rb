558a2e4a3a34518b325b73a5b2a60c453fe12fb8
r_nishio <quick.xp@me.com>
2017-05-13 22:36:28 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,46 @@
 require "csv"
 
 Ship.delete_all
-CSV.foreach('db/ships.csv') do |row|
+CSV.foreach('data/ships.csv') do |row|
   Ship.create(:id => row[0], :ship_type => row[1], :ship_name => row[2])
 end
 
-StaStation.delete_all
-CSV.foreach('db/sta_stations.csv') do |row|
-  StaStation.create(
+Region.delete_all
+header = true
+CSV.foreach('data/mapRegions.csv') do |row|
+  if header
+    header = false
+    next
+  end
+  Region.create(
     :id => row[0],
-    :station_id => row[1],
-    :region_id => row[2],
-    :solar_system_id => row[3],
-    :station_name => row[4]
+    :name => row[1],
   )
 end
+
+SolarSystem.delete_all
+header = true
+CSV.foreach('data/mapSolarSystems.csv') do |row|
+  if header
+    header = false
+    next
+  end
+  SolarSystem.create(
+    :id => row[2],
+    :name => row[3],
+  )
+end
+
+InvItem.delete_all
+header = true
+CSV.foreach('data/invNames.csv') do |row|
+  if header
+    header = false
+    next
+  end
+  InvItem.create(
+    :id => row[0],
+    :name => row[1],
+  )
+end
+
