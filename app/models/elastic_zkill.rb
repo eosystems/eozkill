eee68e7bd1ff9322668c37b935baf5f76e65968b
r_nishio <quick.xp@me.com>
2017-05-13 21:51:55 +0900

    elastic schema

--- app/models/elastic_zkill.rb
+++ app/models/elastic_zkill.rb
@@ -1,38 +1,130 @@
 class ElasticZkill
   include ActiveModel::Model
 
-  @client = nil
+  @client_e = nil
+  @client_z = nil
 
   def initialize
-    @client = Elasticsearch::Client.new({log: true, hosts: {host: 'localhost'}})
+    @client_e = ElasticClient.new
+    @client_z = ZkillClient.new
   end
 
-  def health_check
-    @client.cluster.health
+  def fetch_loss(url)
+    r = @client_z.general_fetch(url)
   end
 
-  def all_index
-    @client.cat.indices
+  def sample
+    @client_z.general_fetch("/losses/regionID/10000002/")
   end
 
-  def create_index(name)
-    @client.indices.create(index: name)
+  def sample2
+    response = @client_z.general_fetch("/losses/regionID/10000002/")
+    binding.pry
+    response.items.each do |item|
+      begin
+        @client_e.create_document("zkill", item["killID"], item)
+      rescue Exception => e
+        Rails.logger.warn e.message
+      end
+    end
   end
 
-  def create_document(index_name, id, body)
-    @client.create({ index: index_name, type: 'info', id: id, body: body })
+  def create_index
+    @client_e.create_index("zkill_loss")
   end
 
-  def search(index_name, query: '*')
-    @client.search(index: index_name, q: query)
+  def put_mapping
+    m = '{
+      "properties": {
+        "killID" : {
+          "type" : "long"
+        },
+        "killTime" : {
+          "type" : "date",
+          "store" : "yes",
+          "format" : "YYYY-mm-dd HH:mm:ss"
+        },
+        "regionID" : {
+          "type" : "long"
+        },
+        "regionName" : {
+          "type" : "string",
+          "store" : "yes"
+          "index" : "not_alanyzed"
+        },
+        "solarSystemID" : {
+          "type" : "long"
+        },
+        "solarSystemName" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "locationID" : {
+          "type" : "locationID"
+        },
+        "locationName" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "shipTypeID" : {
+          "type" : "long
+        },
+        "shipType" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "shipName" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "characterID" : {
+          "type" : "long"
+        },
+        "charactername" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "corporationID" : {
+          "type" : "long"
+        },
+        "corporationName" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "allianceID" : {
+          "type" : "long"
+        },
+         "allianceName" : {
+          "type" : "string",
+          "store" : "yes",
+          "index" : "not_alanyzed"
+        },
+        "damegeTaken" : {
+          "type" : "long"
+        },
+        "totalValue" : {
+          "type" : "long"
+        },
+        "points" : {
+          "type" : "long"
+        },
+        "npc" : {
+          "type" :boolean"
+        }
+
+      }
+    }
+    '
+    @client_e.put_mapping("zkill", "info", m)
   end
 
-  def put_mapping(index_name, type, body)
-    @client.indices.put_mapping index: index_name, type: type, body: body
+  def delete_index
+    @client_e.delete("zkill")
   end
-
-  def delete_mapping(index_name, type)
-    @client.indices.delete_mapping index: index_name, type: type
-  end
-
 end
